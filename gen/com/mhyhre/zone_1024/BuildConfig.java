e2931a85dd16d14d5aaa826eaa787558bede0010
Andrey Tulaykov <mhyhre@gmail.com>
2014-03-12 13:54:44 +0400

    First init commit

---
+++ gen/com/mhyhre/zone_1024/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.mhyhre.zone_1024;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/mhyhre/zone_1024/MainActivity.java
@@ -0,0 +1,22 @@
+package com.mhyhre.zone_1024;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+}
