691eba94ecd22b51998c176a2f725579aab360e8
Andrey Tulaykov <mhyhre@gmail.com>
2014-04-06 22:18:33 +0400

    New branch for animation implementation. Added ApplicationState enum

---
+++ src/com/mhyhre/zone_1024/ApplicationState.java
@@ -0,0 +1,16 @@
+package com.mhyhre.zone_1024;
+
+public enum ApplicationState {
+
+    // Q - Question
+   LOADER,
+   ABOUT,
+   NEW_GAME,
+   SCORES_VIEW,
+   GAME_PROCESS,
+   RESTART_Q,
+   GAME_WIN_SCENE,
+   GAME_OVER_SCENE,
+   TERMINATE,
+   CONTINUE_GAME_Q;
+}
